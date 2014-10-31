99a8b9d106f1db26be4960a879c4cfc404b450c4
davisduan8 <37285451@qq.com>
2014-10-31 09:08:01 +0800

    Revert "10.31"
    This reverts commit cbc85977a3f4afc620b3d1acdc69ac3ea1e3186c.

--- protected/views/doctor/errors.php
+++
@@ -1,5 +0,0 @@
-<h1>操作错误！</h1>
-<p>
-<a href='index.php?r=doctor/index'>返回首页</a>
-</p>
-
--- protected/views/site/dicomurl.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
-?>
-<iframe src="http://jdge.kw120.com/ami/html/WebViewer.html?view&un=kingway&pw=kingway2013!&sui=<?php echo $urlstr;?>" width="100%" height="100%"></iframe>
